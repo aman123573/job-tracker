179a2d7ff3df4c35cbdd0dddec4a4f8035265fdd
Aman Kumar <akmu7835@gmail.com>
2026-06-10 16:40:03 +0530

    update cors for production

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const applicationRoutes = require('./routes/applicationRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173' }));
+// app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: '*' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/applications', applicationRoutes);
